4c4bce914f5ea14d570228be8f3bc100b6a3ba34
Abhishek Mardiya <97448460+abhishekmardiya@users.noreply.github.com>
2022-07-27 16:52:53 +0530

    Create Context_API.jsx

---
+++ Context_API.jsx
@@ -0,0 +1,57 @@
+//Context API Bolier Plate Code
+
+
+
+//context
+import { createContext, useState } from "react";
+
+export const CounterContext = createContext();
+
+export function CounterContextProvider({ children }) {
+  const [count, setCount] = useState(0);
+
+  const handleCount = (inc) => {
+    setCount(count + inc);
+  };
+  return (
+    <CounterContext.Provider value={{ count, handleCount }}>
+      {children}
+    </CounterContext.Provider>
+  );
+}
+
+
+
+//component
+import React, { useContext } from "react";
+import { CounterContext } from "../context/CounterContext";
+
+function Counter() {
+  const { count, handleCount } = useContext(CounterContext);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => handleCount(1)}>Add</button>
+    </div>
+  );
+}
+
+export default Counter;
+
+
+
+//index.js configuration 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CounterContextProvider } from "./context/CounterContext";
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <CounterContextProvider>
+    <App />
+  </CounterContextProvider>
+);
+
